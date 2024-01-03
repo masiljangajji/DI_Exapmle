4978bf8bef5bf4a3a0c965c13a1f953abd379446
masiljangajji <hjk172262@gmail.com>
2024-01-04 02:54:22 +0900

    feat: XML기반 Annotation 사용

--- src/main/java/com/springframework/core/practice/service/ChefCookService.java
+++ src/main/java/com/springframework/core/practice/service/ChefCookService.java
@@ -1,15 +1,18 @@
 package com.springframework.core.practice.service;
 
 import com.springframework.core.practice.domain.chef.Chef;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class ChefCookService {
-    private Chef chef;
+
+    //// Autowired 어노테이션 기반 설정
+    @Autowired
+    // americanChef 이름을 가진 빈을 사용하겠다
+    @Qualifier("americanChef")
+    private  Chef chef;
 
     public ChefCookService() {
-    }
-
-    public void setKoreanChef(Chef chef) {
-        this.chef = chef;
     }
 
     public void makeFood() {
--- src/main/java/com/springframework/core/practice/service/LanguageSpeakService.java
+++ src/main/java/com/springframework/core/practice/service/LanguageSpeakService.java
@@ -1,20 +1,18 @@
 package com.springframework.core.practice.service;
 
 import com.springframework.core.practice.domain.lang.Language;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class LanguageSpeakService {
 
-    private  Language language;
-
-    // setter 주입을 위한 기본 생성자
-    public LanguageSpeakService(){
-    }
-    public LanguageSpeakService(Language language) {
-        this.language = language;
-    }
+    private  final Language language;
 
 
-    public void setLanguage(Language language) {
+    // Autowired 어노테이션 기반 설정
+    @Autowired
+    // english 이름을 가진 빈을 사용하겠다
+    public LanguageSpeakService(@Qualifier("english") Language language) {
         this.language = language;
     }
 
