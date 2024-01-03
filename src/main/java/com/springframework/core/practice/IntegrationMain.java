58e15145121caffacb34eaae57134ccae992a4f6
masiljangajji <hjk172262@gmail.com>
2024-01-04 04:03:41 +0900

    feat: XML없이 Java Configuration으로만 설정

--- src/main/java/com/springframework/core/practice/IntegrationMain.java
+++ src/main/java/com/springframework/core/practice/IntegrationMain.java
@@ -7,41 +7,37 @@ import com.springframework.core.practice.domain.lang.Korean;
 import com.springframework.core.practice.service.ChefCookService;
 import com.springframework.core.practice.service.LanguageSpeakService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IntegrationMain {
 
     public static void main(String[] args) {
 
-        // Context에 등록하는 메타데이터의 형식이 다를 뿐 똑같은 Context에 등록하는 것
-
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                // resources/beans.xml 파일을 읽어서 컨테이너에 객체 등록하겠다.
-                "beans.xml")) {
-
-            English english = context.getBean("english", English.class);
-            Korean korean = context.getBean("korean", Korean.class);
-            english.hello();
-            korean.hello();
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("com.springframework.core.practice");
 
 
-            LanguageSpeakService languageSpeakService =
-                    context.getBean("languageSpeakService", LanguageSpeakService.class);
+        English english = context.getBean("english", English.class);
+        Korean korean = context.getBean("korean", Korean.class);
+        english.hello();
+        korean.hello();
 
-            languageSpeakService.speak();
-        }
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.springframework.core.practice");
+        LanguageSpeakService languageSpeakService =
+                context.getBean("languageSpeakService", LanguageSpeakService.class);
 
-        AmericanChef americanChef=context.getBean("americanChef", AmericanChef.class);
+        languageSpeakService.speak();
+
+
+        AmericanChef americanChef = context.getBean("americanChef", AmericanChef.class);
         americanChef.cook();
 
-        KoreanChef koreanChef=context.getBean("koreanChef", KoreanChef.class);
+        KoreanChef koreanChef = context.getBean("koreanChef", KoreanChef.class);
         koreanChef.cook();
 
         ChefCookService chefCookService = context.getBean("chefCookService", ChefCookService.class);
 
         chefCookService.makeFood();
+
 
     }
 }
